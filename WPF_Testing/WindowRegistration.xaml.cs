833762a8f5e677c5b965bff833674235924eeeb6
Fidyk Dmytro <dima31251@gmail.com>
2020-03-31 16:20:08 +0300

    [4] Added styles and templates for controls

---
+++ WPF_Testing/WindowRegistration.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Testing
+{
+    /// <summary>
+    /// Interaction logic for WindowRegistration.xaml
+    /// </summary>
+    public partial class WindowRegistration : Window
+    {
+        public WindowRegistration()
+        {
+            InitializeComponent();
+        }
+    }
+}
