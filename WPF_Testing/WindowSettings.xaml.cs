38edb8c3a4d7cf162e34077c88100828ea1fd200
koperdasV <52140022+koperdasV@users.noreply.github.com>
2020-06-05 03:05:01 +0300

    Create WindowAuthorization and WindowSettings

---
+++ WPF_Testing/WindowSettings.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Testing
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowSettings.xaml
+    /// </summary>
+    public partial class WindowSettings : Window
+    {
+        public WindowSettings()
+        {
+            InitializeComponent();
+        }
+    }
+}
